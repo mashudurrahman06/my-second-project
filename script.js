5cd81fbcf6f6bc51c34911bd32ce00bfbbca5996
mashudurrahman06 <masudnetit@gmail.com>
2026-02-03 17:24:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ document.getElementById('video').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://www.youtube.com/embed/lJQxYq8lkF0?si=37MOvfkddjz3D8st" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  });
+   document.getElementById('secondVideo').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://www.youtube.com/embed/l-c38BrkaqI?si=TUerC2CHqiiQWefS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  });
+   document.getElementById('thirdVideo').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://www.youtube-nocookie.com/embed/ZrEST63Xi6o?si=hwvMDRpj_dgkNA8v" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  });
